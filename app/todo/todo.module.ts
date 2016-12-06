2c4ff85580b1f9de63bee91360c8262ae8653e8f
danhlee329 <dan.h.lee329@gmail.com>
2016-12-05 16:14:24 -0800

    Didn't add new files, need files for app to run

---
+++ app/todo/todo.module.ts
@@ -0,0 +1,15 @@
+import { TodoService } from './todo.service';
+import { NgModule }            from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { FormsModule }         from '@angular/forms';
+//import { TodoService }         from './todo.service';
+import { TodoListComponent }   from './todo-list.component';
+
+
+@NgModule({
+  imports:      [ CommonModule, FormsModule ],
+  declarations: [ TodoListComponent ],
+  providers:    [ TodoService ],
+  exports:      [ TodoListComponent, CommonModule, FormsModule ]
+})
+export class TodoModule { }
