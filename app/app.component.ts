1fb4afd1820a603889fdfd4111698f3a3e21838e
danhlee329 <dan.h.lee329@gmail.com>
2017-02-21 20:38:43 -0800

    Added class to todolists

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,15 +5,14 @@ import { Component } from '@angular/core';
   template: `    
     <div>
         <h1>Angular Todo - Condo Work</h1>
-        <todo-list listName="Bathroom (Upstairs)"></todo-list>
-        <todo-list listName="Bathroom (Mezzanine)"></todo-list>
-        <todo-list listName="Kitchen"></todo-list>
-        <todo-list listName="Living Room"></todo-list>
-        <todo-list listName="Misc"></todo-list>
+        <div class="TodoModule_Content">
+            <todo-list class="TodoModule_list" listName="Bathroom (Upstairs)"></todo-list>
+            <todo-list class="TodoModule_list" listName="Bathroom (Mezzanine)"></todo-list>
+            <todo-list class="TodoModule_list" listName="Kitchen"></todo-list>
+            <todo-list class="TodoModule_list" listName="Living Room"></todo-list>
+            <todo-list class="TodoModule_list" listName="Misc"></todo-list>
+        </div>
     </div>
 `
 })
-export class AppComponent {
-    
-
-}
+export class AppComponent { }
